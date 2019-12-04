6cc39504ce8293010783fb963875bf5cdc2ba8c4
LongC1019I1 <bachthanhlong.it@gmail.com>
2019-12-05 05:47:51 +0700

    BT

--- session_02_array_function/long/baitap/timgiatrilonnhat/finmax.php
+++ session_02_array_function/long/baitap/timgiatrilonnhat/finmax.php
@@ -1,11 +1,18 @@
 <?php
-$arr = [1,5,23,123,2222,2,2,56];
+$arr = [
+    [1,5,23,123,2222,2,2,56],
+    [23,23,213,45,23,43,]
+];
 
-$max = $arr[0];
+$max = $arr[0][0];
+for ($i=0;$i < count($arr); $i++) {
 
-foreach ($arr as $value) {
-    if ($max<$value) {
-        $max = $value;
+    for ($j=0; $j < count($arr[$i]); $j++)
+    {
+        var_dump( $arr[$i]);
+        if ($max < $arr[$i][$j]){
+            $max = $arr[$i][$j];
+        }
     }
 }
-echo $max;
+echo  $max;
